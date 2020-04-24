c397e05fcf1cc4f2981b52775edbf4777a5d4a49
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2020-04-24 13:50:17 +0200

    Up

--- yt-dl.py
+++ yt-dl.py
@@ -9,18 +9,26 @@ import os
 from pathlib import Path
 
 # Variables
-yt=
+yt=''
 path_download=str(os.path.join(Path.home(), "Downloads"))
+
+#Sub-Programas
+def save_url(url):
+    # Importamos la variable global
+    global yt
+    # La guardamos de forma correcta
+    yt=("'"+url+"'")
 
 # Funciones principales
 def insert_url():
     print("Cual es la URL a descargar: ", end="")
     url=input()
-    url=("'"+url+"'")
-    yt=YouTube(url)
-    return(yt)
+    save_url(url)
+    global yt
+    print("Leyendo URL...")
+    yt_main=YouTube(yt)
     print("¿Es este el video que has seleccionado?")
-    print(yt.title)
+    print(yt_main.title)
     print("[S/N]", end="")
     opcion=input()
     if opcion=='S' or opcion=='s':
@@ -41,45 +49,82 @@ def insert_url():
         os.system("cls")
         main()    
     
-def mostrar_url(yt):
-    print(yt)
-    print("\n")
-    print("Pulsa cualquier tecla para continuar")
-    volver=input()
-    main()
+def mostrar_url():
+    global yt
+    if yt!='':  
+        print("Leyendo URL...")
+        yt_main=YouTube(yt)
+        print("\t" + yt_main.title)
+        print(yt)
+        print("\n")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
+    else:
+        print("ERROR: Introduce antes una URL")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
 
-def v_dl(yt):
-    print("Descargando video...")
-    print(yt.streams.filter(file_extension='mp4').first().download(path_download))
-    print("Video descargado con exito")
-    print("Pulsa cualquier tecla para continuar")
-    volver=input()
-    main()
+def v_dl():
+    global yt
+    if yt!='':
+        print("Leyendo URL...")
+        yt_main=YouTube(yt)
+        print("Descargando video...")
+        print(yt_main.streams.filter(file_extension='mp4').first().download(path_download))
+        print("Video descargado con exito")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
+    else:
+        print("ERROR: Introduce antes una URL")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
 
-def a_dl(yt):
-    print("Descargando audio...")
-    print(yt.streams.filter(only_audio=True).first().download(path_download))
-    print("Audio descargado con exito")
-    print("Pulsa cualquier tecla para continuar")
-    volver=input()
-    main()
+def a_dl():
+    global yt
+    if yt!='':
+        print("Leyendo URL...")
+        yt_main=YouTube(yt)
+        print("Descargando audio...")
+        print(yt_main.streams.filter(only_audio=True).first().download(path_download))
+        print("Audio descargado con exito")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
+    else:
+        print("ERROR: Introduce antes una URL")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
     
-def all_dl(yt):
-    print("Descargar todo...")
-    print(yt.streams.filter(file_extension='mp4').first().download(path_download))
-    print("Video descargado con exito")
-    print(yt.streams.filter(only_audio=True).first().download(path_download))
-    print("Audio descargado con exito")
-    print("Pulsa cualquier tecla para continuar")
-    volver=input()
-    main()
+def all_dl():
+    global yt
+    if yt!='':
+        print("Leyendo URL...")
+        yt_main=YouTube(yt)
+        print("Descargar todo...")
+        print(yt_main.streams.filter(file_extension='mp4').first().download(path_download))
+        print("Video descargado con exito")
+        print(yt_main.streams.filter(only_audio=True).first().download(path_download))
+        print("Audio descargado con exito")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
+    else:
+        print("ERROR: Introduce antes una URL")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
 
 
 # Menu Principal
 def main():
-    #os.system("cls")
+    os.system("cls")
 
-    print("\t \t       YOUTUBE DOWNLOADER")
+    print("\t \t    YOUTUBE DOWNLOADER v1.0")
     print("\t \t \t by Xabierland")
     print("\n")
     print("\t \t \t MENU DE OPCIONES")
@@ -90,11 +135,12 @@ def main():
     print("\t [3] Descargar video de youtube.")
     print("\t [4] Descargar solo audio de youtube.")
     print("")
-    print("\t [5] Descargar tanto video como audio.")
+    print("\t [ERROR] Descargar tanto video como audio.")
     print("")
-    print("\t [8] Proximos cambios.")
-    print("\t [9] Creditos.")
-    print("\t [0] Salir.")
+    print("\t [8] Leer cambios.")
+    print("\t [9] Proximamente.")
+    print("\t [0] Creditos.")
+    print("\t [00] Salir.")
     print("\n")
     print("Opcion", end=": ")
     
@@ -105,31 +151,41 @@ def main():
         insert_url()
     elif option=='2':
         os.system("cls")
-        mostrar_url(yt)
+        mostrar_url()
     elif option=='3':
         os.system("cls")
-        v_dl(yt)
+        v_dl()
     elif option=='4':
         os.system("cls")
-        a_dl(yt)
+        a_dl()
     elif option=='5':
         os.system("cls")
-        all_dl(yt)
+        all_dl()
     elif option=='8':
         os.system("cls")
-        print("CAMBIOS")
-        print("")
+        f= open("changelog.txt","r")
+        print(f.read())
+        print("\n")
         print("Pulsa cualquier tecla para continuar")
         volver=input()
         main()
     elif option=='9':
         os.system("cls")
-        print("CREDITOS")
-        print("")
+        f= open("proximamente.txt","r")
+        print(f.read())
+        print("\n")
         print("Pulsa cualquier tecla para continuar")
         volver=input()
         main()
     elif option=='0':
+        os.system("cls")
+        f= open("creditos.txt","r")
+        print(f.read())
+        print("\n")
+        print("Pulsa cualquier tecla para continuar")
+        volver=input()
+        main()
+    elif option=='00':
         os.system("cls")
         exit
     else:
